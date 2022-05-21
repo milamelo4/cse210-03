4fd2f79fbadbc7faa240af8a2c7dd29edda1ba2d
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-05-20 19:14:20 -0600

    Added methods for director.py
    Only methods, not any code inside the methods are added. Defined the attributes for the director class using access modifiers.

--- director.py
+++ director.py
@@ -7,6 +7,56 @@ class Director:
     """A person who directs the game. 
     
     The responsibility of a Director is to control the sequence of play.
-    Attributes:
-    
+    Attributes:    
+        jumper (Jumper) The character that is showing correct and incorrect guesses.
+        is_playing (boolean): Whether or not to keep playing.
+        secrete_word (SecreteWord) The word that is going to be guessed.
+        terminal_service: For getting and displaying information on the terminal.
     """
+
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self._jumper = Jumper()
+        self._is_playing = True
+        self._secret_word = Secrete_word()
+        self._terminal_service = Terminal_service()
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self._is_playing:
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+
+    def _get_inputs(self):
+        """
+
+        Args:
+            self (Director): An instance of Director.
+        """ 
+        
+
+
+    def _do_updates(self):
+        """
+
+        Args:
+            self (Director): An instance of Director.
+        """
+
+
+
+    def _do_outputs(self):
+        """
+
+        Args:
+            self (Director): An instance of Director.
+        """
