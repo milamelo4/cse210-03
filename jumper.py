01c685214cb475b79409feb9d0deba6e505b5136
milamelo4 <mel19009@byui.edu>
2022-05-24 13:44:18 -0600

    Update jumper.py
    Updated methods in Jumper

--- jumper.py
+++ jumper.py
@@ -1,18 +1,24 @@
 from jumper_parts import jumper_parts
 
-print(jumper_parts[0])
 class Jumper:
     
   """The jumpers displays the parachute drawing """
   
   def __init__(self):
-    # self._letters = self._letter.check_asnswer() 
-    # self._word = self 
-    pass
+    
+    self._jumper_parts = jumper_parts
+    
       
-  
-  def jumper():
-      pass
+  def get_parts(self):    
+    return self._jumper_parts[0]
+    
+  def winner(self):
+    self._won = (self._jumper_parts[6])   
+    return self._won
+    
+  def lost(self):
+    self._lost =  (self._jumper_parts[5])
+    return self._lost
  
 
 
